911faac1de51a9110004f50e55e050354d0e2450
mashiken <tatsumikogorou@gmail.com>
2020-11-08 11:05:33 +0900

    Get the value with a getter

---
+++ sample09/Person.java
@@ -0,0 +1,55 @@
+public class Person{
+	private String name;
+	private int age;
+	private String address;
+
+	public Person(String _name,int _age,String _address) {
+		name = _name;
+		if(_age < 0){
+			age = 0; 
+		}else{
+			age = _age;
+		}
+		address = _address;
+	}
+
+	public void say(){
+		System.out.println("私の名前は" + name + "です。年齢は" + age 
+		+ "歳住所は" + address + "です。");
+	}
+
+	//private設定で直接変更する事は不可
+	//publicメソッドに引数を渡し、受け取った値をメソッド内で代入する
+	public void setName(String _name){
+		name = _name;
+	}
+
+	public void setAge(int _age){
+		// age -2歳等実際にあり得ない年齢の変更をif文で防ぐ
+		if(_age > 0) {
+			age = _age;
+		}else{
+			age = 0;
+		}
+	}
+
+	public void setAddress(String _address){
+		address = _address;
+	}
+	
+	// ゲッターでデータを取得する
+	public String getName(){
+		return name;
+	}
+
+	public int getAge(){
+		return age;
+	}
+
+	public String getAddress(){
+		return address;
+	}
+
+}
+
+
---
+++ sample09/PersonTest.java
@@ -0,0 +1,35 @@
+
+class PersonTest {
+	public static void main(String[] args) {
+		Person[] people = new Person[3];
+		people[0] = new Person("‘¾˜Y", 21, "“Œ‹“s`‹æ");
+		people[1] = new Person("‰Ôq",18,"–kŠC“¹D–ys");
+		people[2] = new Person("‰Ôq",-36,"–kŠC“¹D–ys");
+
+		for(int i = 0; i < people.length; i++){
+			people[i].say();
+		}
+
+		people[1].setName("Ÿ˜Y");
+		people[1].setAge(-17);
+		people[1].setAddress("•Ÿ‰ªŒ§•Ÿ‰ªs");
+		people[2].setName("O˜Y");
+		people[2].setAge(-15);
+		people[2].setAddress("‹{éŒ§å‘äs");
+		
+
+		// •\¦‚ğŒ©‚â‚·‚­‚·‚éˆ×‹ós‚ğo—Í
+		System.out.println();
+		for(int i = 0; i< people.length; i++){
+			System.out.println(people[i].getName() + " " + 
+			people[i].getAge() + " " + people[i].getAddress() );
+		}
+
+		int[] num = new int[2];
+		num[0] = 1;
+		num[1] = 11;
+		for(int i = 0; i< num.length; i++){
+			System.out.println(num[i]);
+		}
+	}
+}
